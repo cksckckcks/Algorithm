a6c07aca8ab2e86d501c7654fa7d206ac33561eb
cksckckcks <125573608+cksckckcks@users.noreply.github.com>
2024-01-01 17:33:41 +0900

    [Silver I] Title: 스티커, Time: 300 ms, Memory: 3228 KB -BaekjoonHub

---
+++ 백준/Silver/9465. 스티커/스티커.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T;
+
+	cin >> T;
+
+	while (T-- > 0) {
+		int n;
+
+		cin >> n;
+
+		int** arr = new int* [2];
+		for (int i = 0; i < 2; ++i)
+			arr[i] = new int[n];
+
+		for (int i = 0; i < 2; i++)
+			for (int j = 0; j < n; j++)
+				cin >> arr[i][j];
+
+		if(n != 1) {
+			arr[0][1] += arr[1][0];
+			arr[1][1] += arr[0][0];
+		}
+
+		for (int i = 2; i < n; i++) {
+			arr[0][i] += max(arr[1][i - 1], arr[1][i - 2]);
+			arr[1][i] += max(arr[0][i - 1], arr[0][i - 2]);
+		}
+		
+		int result = max(arr[0][n - 1], arr[1][n - 1]);
+		cout << result << "\n";
+
+		for (int i = 0; i < 2; ++i)
+			delete[] arr[i];
+		delete[] arr;
+	}
+}
