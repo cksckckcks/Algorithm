47c9becc9b4eb2d39c47a3e484a4c65982cbff0b
정찬 <125573608+cksckckcks@users.noreply.github.com>
2024-01-03 20:50:43 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int N;
+
+    cin >> N;
+
+    int* arr = new int[N];
+    int* dp = new int[N];
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+        dp[i] = 1;
+    }
+
+    for (int i = 1; i < N; i++)
+        for (int j = 0; j < i; j++)
+            if (arr[i] < arr[j])
+                dp[i] = max(dp[i], dp[j] + 1);
+
+    int max = 0;
+    for (int i = 0; i < N; i++)
+        if (max < dp[i])
+            max = dp[i];
+
+    cout << max;
+    delete[] arr;
+    delete[] dp;
+}
