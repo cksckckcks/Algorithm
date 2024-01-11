922844d76707677f9e9a959d7b65982f719a38b8
정찬 <125573608+cksckckcks@users.noreply.github.com>
2024-01-11 16:37:27 +0900

    [Bronze III] Title: 네 수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10824. 네 수/네 수.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main() {
+	string A, B, C, D;
+
+	cin >> A >> B >> C >> D;
+
+	A.append(B);
+	C.append(D);
+
+	long long answer = stoll(A) + stoll(C);
+
+	cout << answer;
+}
