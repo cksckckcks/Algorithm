7598027192169aac8f3764cc51a6430d8444ba66
정찬 <125573608+cksckckcks@users.noreply.github.com>
2024-01-12 23:35:11 +0900

    [Gold I] Title: 택배, Time: 3180 ms, Memory: 33300 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Gold/8980. 택배/택배.py
@@ -0,0 +1,22 @@
+N, C = map(int, input().split())
+M = int(input())
+
+arr = []
+for _ in range(M):
+    arr.append(list(map(int, input().split())))
+
+arr.sort(key = lambda x:x[1])
+
+box = [C] * (N + 1)
+
+cnt = 0
+for s, e, b in arr:
+    _min = C
+    for i in range(s, e):
+        _min = min(_min, box[i])
+    _min = min(_min, b)
+    for i in range(s, e):
+        box[i] -= _min
+    cnt += _min
+
+print(cnt)
