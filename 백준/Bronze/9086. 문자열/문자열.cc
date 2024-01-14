356d7b1533748fde86f1244e3590beca02fb8164
정찬 <125573608+cksckckcks@users.noreply.github.com>
2024-01-14 23:09:30 +0900

    [Bronze V] Title: 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	int T;
+
+	cin >> T;
+
+	while (T--) {
+		string s;
+		
+		cin >> s;
+
+		cout << s[0] << s[s.length() - 1] << '\n';
+	}
+}
