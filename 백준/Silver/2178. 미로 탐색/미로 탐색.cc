e6702221510f01bfd392a95567edf54b1ea4412d
정찬 <125573608+cksckckcks@users.noreply.github.com>
2024-01-20 15:44:23 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2116 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <queue>
+using namespace std;
+
+queue <pair<int, int>> q;
+int arr[102][102] = { 0, };
+bool visited[102][102] = { 0, };
+int path[102][102] = { 0, };
+int N, M, minPath = 100000;
+
+void bfs(int x, int y) {
+	q.push(make_pair(x, y));
+	visited[x][y] = 1;
+	path[x][y]++;
+
+	while (q.empty() != 1) {
+		x = q.front().first;
+		y = q.front().second;
+		q.pop();
+
+		if (x == N && y == M) return;
+
+		if (arr[x - 1][y] == 1 && !visited[x - 1][y]) {
+			q.push(make_pair(x - 1, y));
+			visited[x - 1][y] = 1;
+			path[x - 1][y] = path[x][y] + 1;
+		}
+		if (arr[x + 1][y] == 1 && !visited[x + 1][y]) {
+			q.push(make_pair(x + 1, y));
+			visited[x + 1][y] = 1;
+			path[x + 1][y] = path[x][y] + 1;
+		}
+		if (arr[x][y - 1] == 1 && !visited[x][y - 1]) {
+			q.push(make_pair(x, y - 1));
+			visited[x][y - 1] = 1;
+			path[x][y - 1] = path[x][y] + 1;
+		}
+		if (arr[x][y + 1] == 1 && !visited[x][y + 1]) {
+			q.push(make_pair(x, y + 1));
+			visited[x][y + 1] = 1;
+			path[x][y + 1] = path[x][y] + 1;
+		}
+
+
+	}
+}
+
+int main() {
+	cin >> N >> M;
+
+	for (int i = 1; i <= N; i++) {
+		string s;
+
+		cin >> s;
+		
+		for (int j = 1; j <= M; j++)
+			arr[i][j] = s[j - 1] - '0';
+	}
+	
+	bfs(1, 1);
+	cout << path[N][M];
+}
