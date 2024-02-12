e5a6a94a761e842e73386b92d54cd5bcf7f29830
정찬 <125573608+cksckckcks@users.noreply.github.com>
2024-02-12 23:51:59 +0900

    [Bronze IV] Title: 세수정렬, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2752. 세수정렬/세수정렬.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    vector <int> v;
+    
+    for(int i = 0; i < 3; i++) {
+        int n;
+        
+        cin >> n;
+        
+        v.push_back(n);
+    }
+    sort(v.begin(), v.end());
+    
+    for(int i = 0; i < 3; i++)
+        cout << v[i] << " ";
+}
