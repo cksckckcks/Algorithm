bf76d1e3788a2bbc1d56529bbd35de8db9099e94
정찬 <125573608+cksckckcks@users.noreply.github.com>
2024-05-15 02:08:25 +0900

    [Gold V] Title: Coins, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/3067. Coins/Coins.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int T;
+    
+    cin >> T;
+
+    for (int i = 0; i < T; i++) {
+      vector<int> v;  
+      int dp[10001] = {0, };
+      int N, M;
+      
+      cin >> N;
+      
+       for (int j = 0; j < N; j++) {
+         int n;
+
+         cin >> n;
+         v.push_back(n);
+       }
+      
+      cin >> M;
+
+      for (int i = 0; i < N; i++) {
+        dp[v[i]]++;
+        for (int j = v[i] + 1; j <= M; j++)
+          dp[j] += dp[j - v[i]];
+      }
+       
+        cout << dp[M] << '\n';
+    }
+ }
