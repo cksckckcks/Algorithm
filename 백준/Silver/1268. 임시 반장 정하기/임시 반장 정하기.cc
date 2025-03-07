a4b64e80305c5cb24720507147f077257efa3cc0
정찬 <125573608+cksckckcks@users.noreply.github.com>
2025-03-07 23:32:35 +0900

    [Silver V] Title: 임시 반장 정하기, Time: 8 ms, Memory: 6044 KB -BaekjoonHub

---
+++ 백준/Silver/1268. 임시 반장 정하기/임시 반장 정하기.cc
@@ -0,0 +1,44 @@
+#include <iostream> 
+
+using namespace std; 
+
+int cnt[1010][1010]; 
+int student[1010][10]; 
+
+int main() { 
+	int n; 
+    
+	cin >> n;
+	
+	for(int i = 1; i <= n; i++) 
+		for (int j = 1; j <= 5; j++)
+			cin >> student[i][j];
+	
+	for(int k = 1; k <= 5; k++) { 
+		for(int i = 1; i <= n; i++) { 
+			for(int j = 1; j <= n; j++) { 
+				if(i == j) 
+					continue;
+                
+				if(student[i][k] == student[j][k]) 
+				    cnt[i][j] = 1;
+			} 
+		} 
+	} 
+	
+	int maxcnt = 0; 
+	int ans = 1;
+	for(int i = 1; i <= n; i++) { 
+		int num = 0; 
+        
+		for(int j = 1; j <= n; j++) 
+			num += cnt[i][j]; 
+		
+		if(maxcnt < num) { 
+			maxcnt = num; 
+			ans = i; 
+		} 
+	} 
+    
+	cout << ans; 
+}
