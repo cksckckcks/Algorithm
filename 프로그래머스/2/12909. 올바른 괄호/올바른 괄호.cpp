8ed4eb08fdb511753ad73b218aef741f9dd16c1b
정찬 <125573608+cksckckcks@users.noreply.github.com>
2025-03-25 00:48:11 +0900

    [level 2] Title: 올바른 괄호, Time: 0.55 ms, Memory: 3.87 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+bool solution(string s)
+{
+    stack<char> st;
+
+    for(int i = 0; i < s.length(); i++)
+    {
+        if(s[i] == '(')
+            st.push(s[i]);
+        else if(s[i] == ')')
+        {
+            if(st.empty())
+                return false;
+            else
+                st.pop();
+        }
+    }
+ 
+    return st.empty();
+}
