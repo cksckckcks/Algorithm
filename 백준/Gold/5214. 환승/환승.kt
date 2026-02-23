9ab15681262ee915c867c8a40ce147dca763256c
정찬 <125573608+cksckckcks@users.noreply.github.com>
2026-02-23 17:24:28 +0900

    [Gold II] Title: 환승, Time: 1184 ms, Memory: 243640 KB -BaekjoonHub

---
+++ 백준/Gold/5214. 환승/환승.kt
@@ -0,0 +1,47 @@
+import java.util.LinkedList
+
+fun main() {
+    val (n, k, m) = readln().split(" ").map { it.toInt() }
+    val hyperTubes = Array(m) {
+        readln().split(" ").map { it.toInt() }.toSet()
+    }
+    val stations = Array(n + 1) { mutableListOf<Int>() }
+
+    for (i in 0 until m) {
+        for (stationNumber in hyperTubes[i]) {
+            stations[stationNumber].add(i)
+        }
+    }
+
+    val visited = Array(n + 1) { false }
+    val visitedTubes = Array(m + 1) { false }
+    var ans = -1
+    val q = LinkedList<Pair<Int, Int>>()
+    q.add(Pair(1, 1))
+    visited[1] = true
+
+    while (q.isNotEmpty()) {
+        val (now, cnt) = q.poll()
+
+        if (now == n) {
+            ans = cnt
+            break
+        }
+
+        for (hyperTubeNumber in stations[now]) {
+            if (visitedTubes[hyperTubeNumber]) {
+                continue
+            }
+            visitedTubes[hyperTubeNumber] = true
+            for (stationNumber in hyperTubes[hyperTubeNumber]) {
+                if (!visited[stationNumber]) {
+                    visited[stationNumber] = true
+
+                    q.add(Pair(stationNumber, cnt + 1))
+                }
+            }
+        }
+    }
+
+    println(ans)
+}
